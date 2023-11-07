f749c5b73286c71dee0d0adc153b95229ee3603b
katielaptop <katiesun138@gmail.com>
2023-11-07 10:21:12 -0800

    specified where to implement google.com for maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-//    implementation()
+//    implementation() added the google.com implementation here
 
     val room_version = "2.6.0"
     val lifecycle_version = "2.6.2"
